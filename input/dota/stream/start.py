232977932f5712357a0a716dcce4b6bc80610b2f
Daisy Zhou <daisy@wibidata.com>
2015-04-20 15:22:18 -0700

    Preliminary streaming structure, no validation of all matches getting requested only once ..

--- input/dota/stream/start.py
+++ input/dota/stream/start.py
@@ -1,1 +1,9 @@
 __author__ = 'daisy'
+
+from dota.stream import streamer
+import time
+
+s = streamer.Streamer()
+s.start()
+time.sleep(10)
+s.stop()
--- input/dota/stream/streamer.py
+++ input/dota/stream/streamer.py
@@ -1,29 +1,61 @@
 __author__ = 'daisy'
 
+import dota.local_config
+import http.client
+import json
+import threading
+import time
+
+
 class Streamer:
-  """
-  Processes matches from the Dota 2 web API
-  (see http://dev.dota2.com/showthread.php?t=58317 and
-  https://wiki.teamfortress.com/wiki/WebAPI#Dota_2) and transforms polling
-  requests into a stream.
+    """
+    Processes matches from the Dota 2 web API
+    (see http://dev.dota2.com/showthread.php?t=58317 and
+    https://wiki.teamfortress.com/wiki/WebAPI#Dota_2) and transforms polling
+    requests into a stream.
 
-  This class is not thread-safe.
-  """
+    This class is not thread-safe.
+    """
 
-  def start(self, poll_interval=2000):
-    """
-    Starts the Streamer; may not be started if it has already started.
-    :param poll_interval: Number of milliseconds after which to poll for new
-      matches.  Default is 2000.  Valve suggests rate limiting within
-      applications to at most one request per second.
-    :return:
-    """
-    # TODO
+    def __init__(self):
+        self._started = False
 
-  def stop(self):
-    """
-    Stops the Streamer, closing all connections.  It is fine to stop a Streamer
-    more than once.
-    :return:
-    """
-    # TODO
+    def start(self, poll_interval=500):
+        """
+        Starts the Streamer; may not be started if it has already started.
+        :param poll_interval: Number of milliseconds after which to poll for new
+          matches.  Default is 500.  Valve suggests rate limiting within
+          applications to at most one request per second.
+        :return:
+        """
+        self.poll_interval = poll_interval / 1000
+        self._started = True
+        self._connection = http.client.HTTPConnection("api.steampowered.com")
+        self._poll_thread = threading.Thread(target=self._poll_continuously)
+        self._poll_thread.start()
+
+    def stop(self):
+        """
+        Stops the Streamer.  Closes all connections.
+        :return:
+        """
+        self._started = False
+        self._poll_thread.join()
+        self._connection.close()
+
+    def _poll_continuously(self):
+        """
+        Loops continuously and polls if self._started = True.  Does not return
+        until self._started = False.
+        """
+        # TODO add logic to start at the last requested match; etc.
+        while (self._started == True):
+            self._connection.request(
+                "GET",
+                "/IDOTA2Match_570/GetMatchHistory/V001/?key={key}".format(
+                    key=dota.local_config.DOTA2_API_KEY)
+            )
+            response = self._connection.getresponse()
+            match_history = json.loads(response.read().decode("utf-8"))
+            print(match_history["result"]["total_results"])  # TODO remove print
+            time.sleep(self.poll_interval)
