ae240ffafa83081e82383a5f8f0e70d67741d832
Daisy Zhou <daisy@wibidata.com>
2015-04-21 11:07:03 -0700

    Streamer logic done

--- input/dota/stream/streamer.py
+++ input/dota/stream/streamer.py
@@ -18,7 +18,8 @@ class Streamer:
     """
 
     def __init__(self):
-        self._started = False
+        self.running = False
+        self._most_recent_streamed_match = None
 
     def start(self, poll_interval=500):
         """
@@ -29,7 +30,7 @@ class Streamer:
         :return:
         """
         self.poll_interval = poll_interval / 1000
-        self._started = True
+        self.running = True
         self._connection = http.client.HTTPConnection("api.steampowered.com")
         self._poll_thread = threading.Thread(target=self._poll_continuously)
         self._poll_thread.start()
@@ -39,7 +40,7 @@ class Streamer:
         Stops the Streamer.  Closes all connections.
         :return:
         """
-        self._started = False
+        self.running = False
         self._poll_thread.join()
         self._connection.close()
 
@@ -47,15 +48,47 @@ class Streamer:
         """
         Loops continuously and polls if self._started = True.  Does not return
         until self._started = False.
+
+        Relies on time.sleep to poll, so may fall behind if processing takes too
+        long.
         """
         # TODO add logic to start at the last requested match; etc.
-        while (self._started == True):
+        while self.running:
+            if self._most_recent_streamed_match is None:
+                self._most_recent_streamed_match = \
+                    self._get_recent_match_seq_num()
             self._connection.request(
                 "GET",
-                "/IDOTA2Match_570/GetMatchHistory/V001/?key={key}".format(
-                    key=dota.local_config.DOTA2_API_KEY)
+                "/IDOTA2Match_570/GetMatchHistoryBySequenceNum/V001/"
+                "?key={key}&start_at_match_seq_num={match_seq_num}"
+                .format(
+                    key=dota.local_config.DOTA2_API_KEY,
+                    match_seq_num=self._most_recent_streamed_match + 1
+                )
             )
             response = self._connection.getresponse()
             match_history = json.loads(response.read().decode("utf-8"))
-            print(match_history["result"]["total_results"])  # TODO remove print
+            matches = match_history["result"]["matches"]
+            self._most_recent_streamed_match = \
+                matches[len(matches) - 1]["match_seq_num"]
+            # TODO remove print
+            print("first: {n}".format(n=matches[0]["match_seq_num"]))
+            print("last:  {n}".format(n=self._most_recent_streamed_match))
             time.sleep(self.poll_interval)
+
+    def _get_recent_match_seq_num(self):
+        """
+        :return: A match_seq_num of a recent match to start streaming from.
+        """
+        self._connection.request(
+            "GET",
+            "/IDOTA2Match_570/GetMatchHistory/V001/"
+            "?key={key}"
+            "&matches_requested=1"
+            .format(
+                key=dota.local_config.DOTA2_API_KEY
+            )
+        )
+        response = self._connection.getresponse()
+        decoded = json.loads(response.read().decode("utf-8"))
+        return decoded["result"]["matches"][-1]["match_seq_num"]
