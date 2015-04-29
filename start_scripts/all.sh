4b6077a9e5fedd517ab0c6cab61ff224e6cd4b82
Daisy Zhou <daisy@wibidata.com>
2015-04-29 16:16:48 -0700

    start script fix

--- start_scripts/all.sh
+++ start_scripts/all.sh
@@ -1,3 +1,3 @@
 mkdir -p logs
-python3 -m dotainput.stream.start > logs/stream.log
-python3 -m telegram.processor > logs/processor.log
+python3 -m dotainput.stream.start > logs/stream.log &
+python3 -m telegram.processor > logs/processor.log &
