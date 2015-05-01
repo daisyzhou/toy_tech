ad7b841466cf237d4df2ff793e1dbad514ae91b2
Daisy Zhou <daisy@wibidata.com>
2015-05-01 12:27:35 -0700

    Edited start script to reflect logging changes.

--- start_scripts/all.sh
+++ start_scripts/all.sh
@@ -1,3 +1,2 @@
-mkdir -p logs
-python3 -m dotainput.stream.start > logs/stream.log 2>&1 &
-python3 -m telegram.processor > logs/processor.log 2>&1 &
+python3 -m dotainput.stream.start &
+python3 -m telegram.processor &
