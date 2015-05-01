212c7a0458f08427877c53858cee7519dfed23c2
Daisy Zhou <daisy@wibidata.com>
2015-05-01 12:30:24 -0700

    Reverting start script change.

--- start_scripts/all.sh
+++ start_scripts/all.sh
@@ -1,2 +1,3 @@
-python3 -m dotainput.stream.start &
-python3 -m telegram.processor &
+mkdir -p logs
+python3 -m dotainput.stream.start > logs/stream.log 2>&1 &
+python3 -m telegram.processor > logs/processor.log 2>&1 &
